948a76d62da6126181d3cd82f0c16eefa82c0d5e
VU DINH TUAN ANH <tuananhvd@mz.co.kr>
2024-12-17 17:27:59 +0700

    delete file

--- src/main/java/com/example/exportexcel/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.example.exportexcel.service;
-
-import com.example.exportexcel.entity.Customer;
-
-public interface CustomerService {
-    Customer get(Long id);
-    Customer delete(Long id);
-    Customer save(Customer customer);
-}
